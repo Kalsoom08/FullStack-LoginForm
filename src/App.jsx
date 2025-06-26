a9ba1ba12754abb95bc97db431240323b126ce8c
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-26 14:31:23 +0500

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import Login from './Components/loginUser';
-<<<<<<< HEAD
-// import Register from './Components/RegisterUser';
 
-=======
 import { RegisterUser } from './Components/RegisterUser';
->>>>>>> 8c250663a632940d5c2cdfbd0ef681fafcc2bb36
 const App = () => {
   return (
     <div>
       <Login/>
-<<<<<<< HEAD
-      {/* <Register/> */}
-=======
+
       <RegisterUser />
->>>>>>> 8c250663a632940d5c2cdfbd0ef681fafcc2bb36
+
     </div>
   )
 }
