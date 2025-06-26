196e7897e270f79a0d8aac182dd25bb227012a6f
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-26 10:19:30 +0500

    Create login APi

--- src/API's/authAPI.jsx
+++ src/API's/authAPI.jsx
@@ -4,6 +4,13 @@ import axios from "axios"
 const Api = axios.create({
     baseURL : "http://localhost:4000/api/user"
 });
+
  export const registerUser = async() => {
-    const res = await Api.post('register');
+    const res = await Api.post('/register');
+    return res.data;
+};
+
+export const loginUser = async () => {
+    const res = await Api.post('/login');
+    return res.data;
 }
